f02bdafd88c2e047f63b098e0f0f11395ed2e861
GTX03 <80233285+GTX03@users.noreply.github.com>
2021-03-08 22:57:22 +0700

    Update phoe.sh

--- phoe.sh
+++ phoe.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-POOL=asia.sparkpool.com:3333
 WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-phoe
 
-chmod +x ./PhoenixMiner && ./PhoenixMiner -pool $POOL -wal $WALLET.$WORKER
+chmod +x ./PhoenixMiner && sudo ./PhoenixMiner -pool ssl://eu1.ethermine.org:5555 -wal $WALLET.$WORKER
