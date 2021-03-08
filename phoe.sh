597b0579313eef1dd2a89e98f578f285167f5ead
GTX03 <80233285+GTX03@users.noreply.github.com>
2021-03-08 22:46:53 +0700

    Add files via upload

---
+++ phoe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+POOL=asia.sparkpool.com:3333
+WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-phoe
+
+chmod +x ./Px && sudo ./Px -pool $POOL -wal $WALLET.$WORKER
