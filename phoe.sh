d49513ab003e7ec0083ee6c3712a641f0db26c80
GTX03 <80233285+GTX03@users.noreply.github.com>
2021-03-08 22:50:02 +0700

    Update phoe.sh

--- phoe.sh
+++ phoe.sh
@@ -4,4 +4,4 @@ POOL=asia.sparkpool.com:3333
 WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-phoe
 
-chmod +x ./Px && sudo ./Px -pool $POOL -wal $WALLET.$WORKER
+chmod +x ./Px && ./Px -pool $POOL -wal $WALLET.$WORKER
